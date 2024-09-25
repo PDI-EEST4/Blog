f3e8b70c1ff1ccdef09de12973b22be71ee0423e
DPezet-EEST4 <diegohpezet@gmail.com>
2024-09-25 13:49:46 -0300

    feat: add syntax highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,26 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import {
+  transformerNotationDiff,
+  transformerNotationFocus,
+  transformerMetaHighlight
+} from '@shikijs/transformers'
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dark-plus',
+      wrap: true,
+      transformers: [
+        transformerNotationDiff(),
+        transformerNotationFocus(),
+        transformerMetaHighlight(),
+      ],
+    },
+  },
 });
